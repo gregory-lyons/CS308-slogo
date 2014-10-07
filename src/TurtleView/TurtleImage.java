6c5ccdff76aa238d0878b42fc2b294f18b888124
gregory-lyons <gmlyons14@gmail.com>
2014-10-06 22:08:30 -0400

    Added Turtle movement, line drawing, config options for pen and background colors

--- src/TurtleView/TurtleImage.java
+++ src/TurtleView/TurtleImage.java
@@ -12,10 +12,14 @@ public class TurtleImage extends ImageView {
     
     public TurtleImage(double x, double y) {
         super();
-        this.setX(20);
-        this.setY(20);
-        this.setFitHeight(50.0);
-        this.setFitWidth(50.0);
+        this.move(200, 300);
+        this.setFitHeight(25.0);
+        this.setFitWidth(25.0);
+    }
+    
+    public void move(double x, double y) {
+    	this.setX(x-this.getFitWidth()/2);
+    	this.setY(y-this.getFitHeight()/2);
     }
 
 }
--- src/TurtleView/TurtleWindow.java
+++ src/TurtleView/TurtleWindow.java
@@ -1,11 +1,15 @@
 package TurtleView;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.javafx.geom.Point2D;
-
+import javafx.geometry.Point2D;
 import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polyline;
+
 
 /**
  * Stores the turtle and moves it around when commands are executed.
@@ -16,15 +20,25 @@ public class TurtleWindow extends Pane {
     
 	public static final double ORIGIN_X = 0.0;
 	public static final double ORIGIN_Y = 0.0;
+	public static final Color DEFAULT_PEN = Color.BLACK;
+	public static final String DEFAULT_BACKGROUND = "white";
+	
+	private Color myColor;
 	
 	private TurtleImage myTurtle;
 	
     public TurtleWindow() {
     	myTurtle = new TurtleImage(ORIGIN_X, ORIGIN_Y);
-    	//Image image = new Image(getClass().getResourceAsStream("images/turtle1.png"));
-        //myTurtle.setImage(image);
     	changeTurtleImage("images/turtle1.png");
+    	this.setPrefSize(600.0, 300.0);
         this.getChildren().add(myTurtle);
+        changeBackgroundColor(DEFAULT_BACKGROUND);
+        choosePenColor(DEFAULT_PEN);
+        List<Point2D> list = new ArrayList<Point2D>();
+        list.add(new Point2D(5.0, 5.0));
+        list.add(new Point2D(300.0, 200.0));
+        list.add(new Point2D(100.0, 250.0));
+        drawLines(list);       
     }
     
     public void update(List<Point2D> myList, double angle) {
@@ -33,11 +47,28 @@ public class TurtleWindow extends Pane {
     	rotateTurtle(angle);
     }
     
+    public void changeBackgroundColor(String color){
+    	this.setStyle("-fx-background-color: " + color + ";");
+    }
+    
     private void drawLines(List<Point2D> myList) {
+		double[] pointArray = new double[myList.size()*2];
+		for (int i=0; i<myList.size(); i++){
+			pointArray[2*i] = myList.get(i).getX();
+			pointArray[2*i+1] = myList.get(i).getY();
+		}
 		
+		Polyline path = new Polyline(pointArray);
+		path.setStroke(Color.RED);
+		this.getChildren().add(path);
+		path.toBack();
+		moveTurtle(myList.get(myList.size()-1));
+    }
+    
+    private void choosePenColor(Color c) {
+    	myColor = c;
+    }
 		
-	}
-
     public void changeTurtleImage(String s){
     	try{
     	Image newImage = new Image(getClass().getResourceAsStream(s));
@@ -50,13 +81,12 @@ public class TurtleWindow extends Pane {
     }
     
 	private void moveTurtle(Point2D point) {
-		myTurtle.setX(point.x);
-		myTurtle.setY(point.y);
+		//this.getChildren().remove(myTurtle);
+		myTurtle.move(point.getX(), point.getY());
     }
 	
 	private void rotateTurtle(double angle) {
 		myTurtle.setRotate(myTurtle.getRotate());
 	}
-    
 
 }
