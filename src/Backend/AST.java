7a5068859782fca70a811b3e16a9b5850bbaa102
Ashwin Kommajesula <ashwin.kommajesula@gmail.com>
2014-10-16 13:42:08 -0400

    populate the tree with a while loop

--- src/Backend/AST.java
+++ src/Backend/AST.java
@@ -1,38 +1,53 @@
 package Backend;
 
+import java.util.Queue;
+
 import Nodes.ConstantNode;
 import Nodes.Node;
 
 public class AST {
 
 	private Node current;
-	
-	public AST(Node first){
-		current = first;
-		first.setParent(null);
+	private Queue<Node> nodes;
+
+	public AST(Queue<Node> listOfNodes) {
+		nodes = listOfNodes;
 	}
-	
-	public void checkIfHappy(){
-		if(current.noMoreChildren()){
-			current.update();
-			current = current.getParent();
+
+	public void populateTree() {
+		while (!nodes.isEmpty()) {
+			if (current == null) {
+				current = nodes.poll();
+			}
+			if (current.noMoreChildren()) {
+				current.update();
+				current = current.getParent();
+			} else {
+				Node newNode = nodes.poll();
+				current.addChildren(newNode);
+				current = newNode;
+			}
 		}
 	}
-	
-	
-	public void newNode(Node newNode){
-			newNode.setParent(current);
-			current = newNode;
-	}
-	
-	private boolean checkIfConstant(Node newNode){
-		if(current instanceof ConstantNode){
-			current = current.getParent();
-			return true;
-		}
-		return false;
-	}
-	
-	
-	
+
+//	public void checkIfHappy() {
+//		if (current.noMoreChildren()) {
+//			current.update();
+//			current = current.getParent();
+//		}
+//	}
+//
+//	public void newNode(Node newNode) {
+//		newNode.setParent(current);
+//		current = newNode;
+//	}
+//
+//	private boolean checkIfConstant(Node newNode) {
+//		if (current instanceof ConstantNode) {
+//			current = current.getParent();
+//			return true;
+//		}
+//		return false;
+//	}
+
 }
