60ff717bbe99d0c2444dd204de18ce7adbbace3a
jdc61 <jdc61@duke.edu>
2014-10-20 02:22:49 -0400

    started on boolean nodes

--- src/Nodes/Node.java
+++ src/Nodes/Node.java
@@ -1,21 +1,24 @@
 package Nodes;
 
+import java.util.ArrayList;
+
 
 public abstract class Node {
 	
 	private Node parent;
 	protected double printValue;
+	protected ArrayList<Node> myChildren;
 	
 	public abstract Node update();
 
 	public abstract boolean noMoreChildren();
 	
-	private void setParent(Node newNode){
-		parent = newNode;
+	private void setParent(Node node){
+		parent = node;
 	}
 
-	public void addChildren(Node newNode){
-		newNode.setParent(this);
+	public void addChildren(Node node){
+		node.setParent(this);
 	}
 	
 	public Node getParent(){
