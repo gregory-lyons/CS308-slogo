a159ec214d817778489131f80b6bd5c520aa2027
jdc61 <jdc61@duke.edu>
2014-10-20 01:07:57 -0400

    just implementing some command nodes for the turtle

---
+++ src/Nodes/turtlecommands/BackwardNode.java
@@ -0,0 +1,31 @@
+package Nodes.turtlecommands;
+
+import Backend.Turtle;
+import Nodes.ConstantNode;
+import Nodes.Node;
+
+public class BackwardNode extends CommandNode {
+	
+	public BackwardNode(Turtle myTurtle) {
+		super(myTurtle);
+	}
+	
+	@Override
+	public Node update() {
+		printValue = ((ConstantNode) left).returnData();
+		turtle.setLocation(turtle.nextLocation(printValue, 0));
+		return left;
+	}
+
+	@Override
+	public void addChildren(Node newNode) {
+		super.addChildren(newNode);
+		left = newNode;
+	}
+
+	@Override
+	public boolean noMoreChildren() {
+		return (left instanceof ConstantNode);
+	}
+
+}
