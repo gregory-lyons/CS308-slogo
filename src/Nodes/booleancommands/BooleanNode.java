e49bae6720cb0f6d5b30eaf9d72e56c4dbe0e28e
jdc61 <jdc61@duke.edu>
2014-10-21 21:13:31 -0400

    refactoring of booleanNodes and addition of GreaterNode.java and EqualsNode.java

--- src/Nodes/booleancommands/BooleanNode.java
+++ src/Nodes/booleancommands/BooleanNode.java
@@ -1,10 +1,25 @@
 package Nodes.booleancommands;
 
+import Nodes.ConstantNode;
 import Nodes.Node;
 
 public abstract class BooleanNode extends Node{
 
 	protected Node left;
 	protected Node right;
+	
+	
+	public boolean noMoreChildren() {
+		// TODO Auto-generated method stub
+		return (right instanceof ConstantNode);
+	}
+	
+	public void addChildren(Node newNode) {
+		super.addChildren(newNode);
+		if (left == null)
+			left = newNode;
+		else
+			right = newNode;
+	}
 
 }
