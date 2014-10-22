13cc8c9cba140bea1f9b1defe700e95291028070
jdc61 <jdc61@duke.edu>
2014-10-21 21:03:04 -0400

    GreaterNode.java

---
+++ src/Nodes/booleancommands/GreaterNode.java
@@ -0,0 +1,32 @@
+package Nodes.booleancommands;
+
+import Nodes.ConstantNode;
+import Nodes.Node;
+
+public class GreaterNode extends BooleanNode {
+	
+	@Override
+	public Node update() {
+		if (((ConstantNode) left).returnData() > ((ConstantNode) right)
+				.returnData())
+			printValue = 1;
+		else
+			printValue = 0;
+		return new ConstantNode(printValue);
+	}
+
+	@Override
+	public boolean noMoreChildren() {
+		// TODO Auto-generated method stub
+		return (right instanceof ConstantNode);
+	}
+
+	public void addChildren(Node newNode) {
+		super.addChildren(newNode);
+		if (left == null)
+			left = newNode;
+		else
+			right = newNode;
+	}
+
+}
