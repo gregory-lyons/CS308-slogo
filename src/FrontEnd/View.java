e8d1de23ca72c7f11a91bdfdc3b2266d7c8ecd0c
gregory-lyons <gmlyons14@gmail.com>
2014-10-22 17:41:38 -0400

    Updated arrow handler

--- src/FrontEnd/View.java
+++ src/FrontEnd/View.java
@@ -54,191 +54,198 @@ import resources.languages.*;
  *
  */
 public class View {	
-	
-    public static final String DEFAULT_RESOURCE_PACKAGE = "resources.languages/Languages";
-    public static final int BUTTON_WIDTH = 200;
-    public static final int BUTTON_HEIGHT = 40;
-    public static final Dimension DEFAULT_SIZE = new Dimension(1200, 600);
-    public static final double DIALOG_WIDTH = 200;
-    public static final double DIALOG_HEIGHT = 100;
-    public static final boolean DEFAULT_GRIDLINES = true;
-    
-    private Scene myScene;
-    private Controller myController;
-    private ResourceBundle myResources;
 
-    private HBox languageSelectorHBox = new HBox();
-    private VBox myVBox = new VBox();
-    private HBox usercmdHBox = new HBox();
-    private HBox uservrbHBox = new HBox();
-    private HBox bottomHBox = new HBox();
-    private VBox myInnerVBox = new VBox();
-    private VBox centerVBox = new VBox();
-    
-    private CommandLine myCommandLine;
-    private HistoryBox myHistoryBox;
-    private TurtleWindow myTurtleWindow;
-    private List<TurtleImage> myActives;
-    
-    private CommandFactory myCommandFactory;    
-    private EnterCommand myEnterCommand;
-    
-    private UserCommands dropdownCommandMenu;
-    private UserVariables dropdownVariablesMenu;
-    private LanguageSelector myLanguageSelector;
-    private TurtleInformation myTurtleInformation;
-    private List<String> myButtonLabels = new ArrayList<String>(Arrays.asList("Forward", "Left", "Right", "Home", "ClearScreen", "SetPosition", "SetTowards"));
-    private List<SuperCommand> myButtons;
-    
-    /**
-     * Constructs the view
-     * @param language
-     */
-    public View() {           	  	
-        myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE);
-        BorderPane root = new BorderPane();
-        
-        myTurtleInformation = new TurtleInformation();
-        myLanguageSelector = new LanguageSelector(BUTTON_WIDTH, root);
-        dropdownCommandMenu = new UserCommands(myResources.getString("DropdownMenuDefault"), BUTTON_WIDTH);
-        dropdownVariablesMenu = new UserVariables(myResources.getString("DropdownMenuDefault"), BUTTON_WIDTH);
-        makeTextAreas();
-        myCommandFactory = new CommandFactory(myCommandLine, myHistoryBox);
-        myTurtleWindow = new TurtleWindow();
-        myActives = myTurtleWindow.getActiveTurtles();
-        myButtons = new ArrayList<SuperCommand>();
-        makeEnterButton();
+	public static final String DEFAULT_RESOURCE_PACKAGE = "resources.languages/Languages";
+	public static final int BUTTON_WIDTH = 200;
+	public static final int BUTTON_HEIGHT = 40;
+	public static final Dimension DEFAULT_SIZE = new Dimension(1200, 600);
+	public static final double DIALOG_WIDTH = 200;
+	public static final double DIALOG_HEIGHT = 100;
+	public static final boolean DEFAULT_GRIDLINES = true;
 
-        myVBox.getChildren().add(myTurtleInformation.getVBox());
-        languageSelectorHBox.getChildren().add(myLanguageSelector.getComboBox());
-        languageSelectorHBox.getChildren().add(myLanguageSelector.getButton());
-        /*
+	private Scene myScene;
+	private Controller myController;
+	private ResourceBundle myResources;
+
+	private HBox languageSelectorHBox = new HBox();
+	private VBox myVBox = new VBox();
+	private HBox usercmdHBox = new HBox();
+	private HBox uservrbHBox = new HBox();
+	private HBox bottomHBox = new HBox();
+	private VBox myInnerVBox = new VBox();
+	private VBox centerVBox = new VBox();
+
+	private CommandLine myCommandLine;
+	private HistoryBox myHistoryBox;
+	private ArrowKeyHandler myArrowHandler;
+	private TurtleWindow myTurtleWindow;
+	private List<TurtleImage> myActives;
+
+	private CommandFactory myCommandFactory;    
+	private EnterCommand myEnterCommand;
+
+	private UserCommands dropdownCommandMenu;
+	private UserVariables dropdownVariablesMenu;
+	private LanguageSelector myLanguageSelector;
+	private TurtleInformation myTurtleInformation;
+	private List<String> myButtonLabels = new ArrayList<String>(Arrays.asList("Forward", "Left", "Right", "Home", "ClearScreen", "SetPosition", "SetTowards"));
+	private List<SuperCommand> myButtons;
+
+	/**
+	 * Constructs the view
+	 * @param language
+	 */
+	public View() {           	  	
+		myResources = ResourceBundle.getBundle(DEFAULT_RESOURCE_PACKAGE);
+		BorderPane root = new BorderPane();
+		myArrowHandler = new ArrowKeyHandler();
+
+		myTurtleInformation = new TurtleInformation();
+		myLanguageSelector = new LanguageSelector(BUTTON_WIDTH, root);
+		dropdownCommandMenu = new UserCommands(myResources.getString("DropdownMenuDefault"), BUTTON_WIDTH);
+		dropdownVariablesMenu = new UserVariables(myResources.getString("DropdownMenuDefault"), BUTTON_WIDTH);
+		makeTextAreas();
+		myCommandFactory = new CommandFactory(myCommandLine, myHistoryBox);
+		myTurtleWindow = new TurtleWindow();
+		myActives = myTurtleWindow.getActiveTurtles();
+		myButtons = new ArrayList<SuperCommand>();
+		makeEnterButton();
+
+		myVBox.getChildren().add(myTurtleInformation.getVBox());
+		languageSelectorHBox.getChildren().add(myLanguageSelector.getComboBox());
+		languageSelectorHBox.getChildren().add(myLanguageSelector.getButton());
+		/*
         Hyperlink myHyperlink = new Hyperlink("FrontEnd/help.html");
         TextFlow flow = new TextFlow(myHyperlink);
         final WebView webView = WebViewBuilder.create().prefHeight(450).prefWidth(1000).build();
         myHyperlink.setOnAction(event -> {
             webView.getEngine().load("FrontEnd/help.html");
         });
-        */
-        root.setTop(languageSelectorHBox);
-        bottomHBox.getChildren().add(myCommandLine); 
-        usercmdHBox.getChildren().add(dropdownCommandMenu.getComboBox());
-        usercmdHBox.getChildren().add(dropdownCommandMenu.getButton());
-        uservrbHBox.getChildren().add(dropdownVariablesMenu.getComboBox());
-        uservrbHBox.getChildren().add(dropdownVariablesMenu.getButton());
+		 */
+		root.setTop(languageSelectorHBox);
+		bottomHBox.getChildren().add(myCommandLine); 
+		usercmdHBox.getChildren().add(dropdownCommandMenu.getComboBox());
+		usercmdHBox.getChildren().add(dropdownCommandMenu.getButton());
+		uservrbHBox.getChildren().add(dropdownVariablesMenu.getComboBox());
+		uservrbHBox.getChildren().add(dropdownVariablesMenu.getButton());
 
-        HBox penBox = new HBox();
-        penBox.getChildren().addAll(new PenColorBox(), new Text(myResources.getString("PenColor")));
-        HBox backgroundBox = new HBox();
-        backgroundBox.getChildren().addAll(new BackgroundColorBox(myTurtleWindow), new Text("   Background Color"));
-        HBox imageBox = new HBox();
-        imageBox.getChildren().addAll(new TurtleImageBox(myTurtleWindow), new Text("   Turtle Image"));
-        HBox gridBox = new HBox();
-        gridBox.getChildren().addAll(new GridCheckBox(myTurtleWindow), new Text("   Display Grid Lines"));
+		HBox penBox = new HBox();
+		penBox.getChildren().addAll(new PenColorBox(), new Text(myResources.getString("PenColor")));
+		HBox backgroundBox = new HBox();
+		backgroundBox.getChildren().addAll(new BackgroundColorBox(myTurtleWindow), new Text("   Background Color"));
+		HBox imageBox = new HBox();
+		imageBox.getChildren().addAll(new TurtleImageBox(myTurtleWindow), new Text("   Turtle Image"));
+		HBox gridBox = new HBox();
+		gridBox.getChildren().addAll(new GridCheckBox(myTurtleWindow), new Text("   Display Grid Lines"));
 
-        myVBox.getChildren().addAll(usercmdHBox, uservrbHBox);
-        myVBox.getChildren().add(myActives.get(0).getPenOptions());
-        myVBox.getChildren().addAll(backgroundBox, imageBox, gridBox);
-        root.setCenter(centerVBox);
-        centerVBox.getChildren().add(myHistoryBox);
-        myInnerVBox.getChildren().add(myEnterCommand.getButton());
-        myInnerVBox.getChildren().add(makeClearButton());
-        bottomHBox.getChildren().add(1, myInnerVBox);       
-        root.setBottom(bottomHBox);
-        
-        for (String button : myButtonLabels) {
-        	SuperCommand sc = myCommandFactory.makeCommand(button, myResources.getString(button));
-        	myButtons.add(sc);
-            myVBox.getChildren().add(sc.getButton());
-        }
-        root.setRight(myVBox);
-        root.setLeft(myTurtleWindow);
-        
-        myScene = new Scene(root, DEFAULT_SIZE.width, DEFAULT_SIZE.height);
-        myTurtleWindow.requestFocus();
-        ArrowKeyHandler arrowHandler = new ArrowKeyHandler();
-        
-        myScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
+		myVBox.getChildren().addAll(usercmdHBox, uservrbHBox);
+		myVBox.getChildren().add(myActives.get(0).getPenOptions());
+		myVBox.getChildren().addAll(backgroundBox, imageBox, gridBox);
+		root.setCenter(centerVBox);
+		centerVBox.getChildren().add(myHistoryBox);
+		myInnerVBox.getChildren().add(myEnterCommand.getButton());
+		myInnerVBox.getChildren().add(makeClearButton());
+		bottomHBox.getChildren().add(1, myInnerVBox);       
+		root.setBottom(bottomHBox);
+
+		for (String button : myButtonLabels) {
+			SuperCommand sc = myCommandFactory.makeCommand(button, myResources.getString(button));
+			myButtons.add(sc);
+			myVBox.getChildren().add(sc.getButton());
+		}
+		root.setRight(myVBox);
+		root.setLeft(myTurtleWindow);
+
+		myScene = new Scene(root, DEFAULT_SIZE.width, DEFAULT_SIZE.height);
+		myTurtleWindow.requestFocus();
+		myScene.setOnKeyPressed(event -> sendArrowCommand(event));
+		/*  @Override
             public void handle (KeyEvent thisKey) {
                 System.out.println(thisKey.getCode());
                 String instruction = arrowHandler.makeInstruction(thisKey.getCode());
                 myEnterCommand.notifyObservers(instruction);
-                //myTurtleWindow.startMovingTurtle(thisKey);
+
                 thisKey.consume();
             }   
-        });
-    }
+        });*/
+	}
 
-    /**
-     * Creates a clear button that clears the command line when pressed. This is not included in the command
-     * factory because it is not a subclass of SuperCommand since it is very simple and doesn't need the features
-     * that SuperCommand has.
-     * @return clear button
-     */
-    private Button makeClearButton () {
-        Button myClearButton = new Button();
-        myClearButton.setText(myResources.getString("Clear"));
-        myClearButton.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        myClearButton.setOnAction(new EventHandler<ActionEvent>() {                  
-            public void handle (ActionEvent event) {
-                myCommandLine.clear();
-            }
-        });
-        return myClearButton;   
-    }
+	private void sendArrowCommand(KeyEvent ke){
+        //myTurtleWindow.startMovingTurtle(ke);
+		String instruction = myArrowHandler.makeInstruction(ke.getCode());
+		myEnterCommand.notifyObservers(instruction);
+		ke.consume();
+	}
 
-    /**
-     * Initializes the command line and the history box.
-     */
-    private void makeTextAreas () {
-        myCommandLine = new CommandLine(myResources.getString("CommandLineDefault"));
-        
-        myHistoryBox = new HistoryBox(dropdownCommandMenu, myResources.getString("HistoryBoxDefault"));
-        myHistoryBox.setEditable(false);
-    }
-    
-    /**
-     * Creates the Enter Button. This is not created in the button for loop above because it is in a
-     * different location than the other regular command buttons.
-     */
-    private void makeEnterButton() {
-        myEnterCommand = (EnterCommand) myCommandFactory.makeCommand("Enter", myResources.getString("Enter"));
-    }
-    
-    public Stage makeErrorDialog(String message){
-    	Stage dialog = new Stage();
-    	dialog.initStyle(StageStyle.UTILITY);
-    	Scene errorScene = new Scene(new Group(new Text(DIALOG_WIDTH/2, DIALOG_HEIGHT/2, message)), DIALOG_WIDTH, DIALOG_HEIGHT);
-    	dialog.setScene(errorScene);
-    	return dialog;
-    }
+	/**
+	 * Creates a clear button that clears the command line when pressed. This is not included in the command
+	 * factory because it is not a subclass of SuperCommand since it is very simple and doesn't need the features
+	 * that SuperCommand has.
+	 * @return clear button
+	 */
+	private Button makeClearButton () {
+		Button myClearButton = new Button();
+		myClearButton.setText(myResources.getString("Clear"));
+		myClearButton.setMinSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+		myClearButton.setOnAction(new EventHandler<ActionEvent>() {                  
+			public void handle (ActionEvent event) {
+				myCommandLine.clear();
+			}
+		});
+		return myClearButton;   
+	}
 
-    /**
-     * 
-     * @return the scene
-     */
-    public Scene getScene () {
-        return myScene;
-    }
-    
-    public TurtleWindow getTurtleWindow() {
-    	return myTurtleWindow;
-    }
-    
-    public TurtleInformation getTurtleInfo() {
-    	return myTurtleInformation;
-    }
-    
-    public void addController (Controller c) {
-    	myController = c;
-        dropdownCommandMenu.addObserver(myController);
-        for (SuperCommand button: myButtons) {
-        	button.addObserver(myController);
-        }
-        myEnterCommand.addObserver(myController);
-        
-    }
+	/**
+	 * Initializes the command line and the history box.
+	 */
+	private void makeTextAreas () {
+		myCommandLine = new CommandLine(myResources.getString("CommandLineDefault"));
+
+		myHistoryBox = new HistoryBox(dropdownCommandMenu, myResources.getString("HistoryBoxDefault"));
+		myHistoryBox.setEditable(false);
+	}
+
+	/**
+	 * Creates the Enter Button. This is not created in the button for loop above because it is in a
+	 * different location than the other regular command buttons.
+	 */
+	private void makeEnterButton() {
+		myEnterCommand = (EnterCommand) myCommandFactory.makeCommand("Enter", myResources.getString("Enter"));
+	}
+
+	public Stage makeErrorDialog(String message){
+		Stage dialog = new Stage();
+		dialog.initStyle(StageStyle.UTILITY);
+		Scene errorScene = new Scene(new Group(new Text(DIALOG_WIDTH/2, DIALOG_HEIGHT/2, message)), DIALOG_WIDTH, DIALOG_HEIGHT);
+		dialog.setScene(errorScene);
+		return dialog;
+	}
+
+	/**
+	 * 
+	 * @return the scene
+	 */
+	public Scene getScene () {
+		return myScene;
+	}
+
+	public TurtleWindow getTurtleWindow() {
+		return myTurtleWindow;
+	}
+
+	public TurtleInformation getTurtleInfo() {
+		return myTurtleInformation;
+	}
+
+	public void addController (Controller c) {
+		myController = c;
+		dropdownCommandMenu.addObserver(myController);
+		for (SuperCommand button: myButtons) {
+			button.addObserver(myController);
+		}
+		myEnterCommand.addObserver(myController);
+
+	}
 
 
 
