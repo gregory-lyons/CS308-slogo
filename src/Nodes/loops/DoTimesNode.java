93c16d8a1c5070f3db97de9c1383f956e225ad28
Rica Zhang <ricagz@yahoo.com>
2014-10-26 07:18:01 -0400

    refactor

--- src/Nodes/loops/DoTimesNode.java
+++ src/Nodes/loops/DoTimesNode.java
@@ -1,12 +1,5 @@
 package Nodes.loops;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
-
-import Nodes.ConstantNode;
-import Nodes.ListEndNode;
-import Nodes.Node;
-
 public class DoTimesNode extends LoopNode {
 	
 
--- src/Nodes/loops/RepeatNode.java
+++ src/Nodes/loops/RepeatNode.java
@@ -1,11 +1,5 @@
 package Nodes.loops;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-
-import Nodes.*;
-
 public class RepeatNode extends LoopNode {
 
 
