e84457b9eb32c7496fc745661b3e843b9709cc1d
gregory-lyons <gmlyons14@gmail.com>
2014-10-26 07:24:23 -0400

    Added boundaries

--- src/FrontEnd/Boundary.java
+++ src/FrontEnd/Boundary.java
@@ -1,75 +1,27 @@
 package FrontEnd;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import javafx.geometry.Point2D;
 
 public class Boundary {
 	
 	private double myHeight;
 	private double myWidth;
-	private boolean xBig;
-	private boolean xSmall;
-	private boolean yBig;
-	private boolean ySmall;
-	private double newX;
-	private double newY;
+
 	
 	public Boundary (double w, double h) {
 		myWidth = w;
 		myHeight = h;
-		xBig = false;
-		xSmall = false;
-		yBig = false;
-		ySmall = false;
-	}
-
-	public List<Point2D> checkWrap(Point2D currentPoint, Point2D newPoint){
-		
-		List<Point2D> newPoints = new ArrayList<Point2D>();
-		
-		return newPoints;
-		/*double dx = newPoint.getX() - currentPoint.getX();
-		double dy = currentPoint.getY() - newPoint.getY();
-		double theta = Math.toDegrees(Math.atan2(dy, dx));
-		newX = newPoint.getX();
-		newY = newPoint.getY();
-		List<Point2D> points = new ArrayList<Point2D>();
-		xBig = newPoint.getX()>myWidth;
-		yBig = newPoint.getY()>myWidth;
-		xSmall = newPoint.getX()<0;
-		ySmall = newPoint.getY()<0;
-		newX = newPoint.getX() % myWidth;
-		newY = newPoint.getY() % myHeight;
-		
-		if (xBig){
-			double px = myWidth;
-			double py = myHeight/2 - myWidth*Math.tan(Math.toRadians(theta));
-			Point2D p = new Point2D(px, py);
-			
-			double qx = 0;
-			double qy = myHeight/2 + myWidth*Math.tan(Math.toRadians(theta));
-			Point2D q = new Point2D(qx, qy);
-			
-			newX = newPoint.getX() % myWidth;
-			newY = newPoint.getY() % myHeight;
-			if (newX < 0) newX+= myWidth;
-			if (newY < 0) newY+= myHeight;
-			Point2D location = new Point2D(newX, newY);
-			points.add(p);
-			points.add(q);
-			points.add(location);
-		}
-		else{
-			points.add(newPoint);
-		}
-		if (xSmall){
-			
-		}
-		
-			return points;*/
 
 	}
 
+	public Point2D checkBoundary(Point2D checkPoint) {
+		double x = checkPoint.getX();
+		double y = checkPoint.getY();
+		if (x<0) x = 0;
+		if (y<0) y = 0;
+		if (x>myWidth) x = myWidth;
+		if (y>myHeight) y = myHeight;
+		return new Point2D(x,y);
+		
+	}
 }
