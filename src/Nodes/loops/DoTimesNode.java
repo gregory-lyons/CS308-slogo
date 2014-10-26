fc4c59a9fae28b6045516dd0e15fe87258e5c125
jdc61 <jdc61@duke.edu>
2014-10-26 06:47:56 -0400

    refactor the loops

--- src/Nodes/loops/DoTimesNode.java
+++ src/Nodes/loops/DoTimesNode.java
@@ -9,27 +9,6 @@ import Nodes.Node;
 
 public class DoTimesNode extends LoopNode {
 	
-	/*public double counter;
-	
-	public void addChildren(Node newNode) {
-		if (myChildren.isEmpty()) {
-			counter = ((ConstantNode) newNode).returnData();
-		}
-		super.addChildren(newNode);
-	}
-	
-	@Override
-	public boolean isFinished() {
-		return false;
-	}
-	
-	public Queue<Node> iterator(Queue<Node> copy) {
-		Queue<Node> ret = new ArrayDeque<Node>();
-		copy.poll();
-		while (!(copy.peek() instanceof ListEndNode)) {
-			ret.add(copy.poll());
-		}
-		return ret;
-	}*/
+
 
 }
--- src/Nodes/loops/RepeatNode.java
+++ src/Nodes/loops/RepeatNode.java
@@ -8,28 +8,6 @@ import Nodes.*;
 
 public class RepeatNode extends LoopNode {
 
-	
-	/*public double counter;
-	
-	public void addChildren(Node newNode) {
-		if (myChildren.isEmpty()) {
-			counter = ((ConstantNode) newNode).returnData();
-		}
-		super.addChildren(newNode);
-	}
-	
-	@Override
-	public boolean isFinished() {
-		return false;
-	}
-	
-	public Queue<Node> iterator(Queue<Node> copy) {
-		Queue<Node> ret = new ArrayDeque<Node>();
-		copy.poll();
-		while (!(copy.peek() instanceof ListEndNode)) {
-			ret.add(copy.poll());
-		}
-		return ret;
-	}*/
+
 
 }
