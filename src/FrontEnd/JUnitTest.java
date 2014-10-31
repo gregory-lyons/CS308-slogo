ab7686f179566c62064d25192bc59e383f596e94
gregory-lyons <gmlyons14@gmail.com>
2014-10-31 19:24:13 -0400

    Fixed JUnit tests using code from StackOverflow

--- src/FrontEnd/JUnitTest.java
+++ src/FrontEnd/JUnitTest.java
@@ -1,18 +1,26 @@
 package FrontEnd;
 
 import static org.junit.Assert.*;
-import FrontEnd.View;
 
+import java.lang.reflect.Field;
+
+import FrontEnd.View;
+import TurtleView.TurtleWindow;
+
+import org.junit.Rule;
 import org.junit.Test;
 
 public class JUnitTest {
 
+	@Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+	
 	public JUnitTest() {
 		
 	}
 	
 	@Test
 	public void checkViewInit(){
+		
 		View v = new View();
 		assertNotNull(v.getScene());
 		assertNotNull(v.getTurtleInfo());
@@ -30,6 +38,27 @@ public class JUnitTest {
 	public void checkLanguage(){
 		View v = new View();
 		assertEquals(v.getCurrentLanguage(), ViewConstants.DEFAULT_LANGUAGE);
+	}
+	
+	@Test
+	public void addTurtle(){
+		View v = new View();
+		TurtleWindow tw = v.getTurtleWindow();
+		tw.makeTurtle();
+		Object value = null;
+		try {
+			Field turtleCount = null;
+			turtleCount = tw.getClass().getDeclaredField("numTurtles");
+			turtleCount.setAccessible(true);
+			try {
+				value = turtleCount.get(tw);
+			} catch (IllegalArgumentException | IllegalAccessException e) {
+			}
+			
+		} catch (NoSuchFieldException | SecurityException e) {
+			
+		}
+        assertEquals(value, tw.START_TURTLES+1);
 	}
 
 }
